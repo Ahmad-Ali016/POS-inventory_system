ed747b95a7221e824d921e899bac3bf38c1b9eca
Ahmad-Ali <mhrali016@gmail.com>
2026-02-13 01:38:10 +0500

    Users list view to show all the registered and available users

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,8 @@ from .serializers import UserSerializer
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+# This view only allows GET requests to see all users
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()           # Fetch all users from the DB
+    serializer_class = UserSerializer       # Use the translator we built earlier
